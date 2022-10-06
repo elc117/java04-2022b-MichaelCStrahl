3ea9ffb964c96d63754a53e7c2140a5a264a95d7
Michael Strahl <michaelstrahl.sm@gmail.com>
2022-10-06 14:03:24 -0300

    Atualização de código e correção de bugs nos métodos userExists() e getContactInfors()

--- Laboratory.java
+++ Laboratory.java
@@ -1,76 +1,72 @@
 import java.util.ArrayList;
 
 public class Laboratory {
-  private String name;
-  private ArrayList<Student> students;
-  private ArrayList<Professor> professors;
+    private String name;
+    private ArrayList<Student> students;
+    private ArrayList<Professor> professors;
 
-  public Laboratory() {
-  }
-
-  public Laboratory(String name) {
-    this.name = name;
-    this.students = new ArrayList<Student>();
-    this.professors = new ArrayList<Professor>();
-  }
-
-  public String getName() {
-    return this.name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public void addMember(Student s) {
-    this.students.add(s);
-  }
-
-  public void addMember(Professor p) {
-    this.professors.add(p);
-  }
-  
-  public ArrayList<String> getContactInfos() {
-    // COMPLETE-ME
-    // Construa um ArrayList<String> contendo informações de contato (ContactInfo)
-    // de cada um dos estudantes e professores
-    ArrayList<String> contactInfo = new ArrayList<String>();
-
-    for (int i = 0 ; i < this.professors.size() + this.students.size(); i++) {
-      int countStudents = this.students.size();
-      int countProfessors = this.professors.size();
-
-      while(countStudents > 0) {
-        contactInfo.add(this.students.get(countStudents - 1).getContactInfo());
-        countStudents--;
-      }
-
-      while(countProfessors > 0) {
-        contactInfo.add(this.professors.get(countProfessors - 1).getContactInfo());
-        countProfessors--;
-      }
+    public Laboratory() {
     }
 
-    return contactInfo;
+    public Laboratory(String name) {
+        this.name = name;
+        this.students = new ArrayList<Student>();
+        this.professors = new ArrayList<Professor>();
+    }
 
-  }
+    public String getName() {
+        return this.name;
+    }
 
-  public boolean userExists(String userId) {
-    // COMPLETE-ME
-    // Verifique se existe o userId na lista de estudantes ou de professores
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addMember(Student s) {
+        this.students.add(s);
+    }
+
+    public void addMember(Professor p) {
+        this.professors.add(p);
+    }
     
-    if (this.professors.get(0).getUserId() == userId) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+    public ArrayList<String> getContactInfos() {
+        // COMPLETE-ME
+        // Construa um ArrayList<String> contendo informações de contato (ContactInfo)
+        // de cada um dos estudantes e professores
+        ArrayList<String> contactInfo = new ArrayList<String>();
 
-  public int countMembers() {
-    // COMPLETE-ME
-    // Retorne o total de membros do laboratório (estudantes e professores)
-    return this.professors.size() + this.students.size();
-  }
+        for (int i = 0; i < this.students.size(); i++) {
+            contactInfo.add(this.students.get(i).getContactInfo());
+        }
+
+        for (int i = 0; i < this.professors.size(); i++) {
+            contactInfo.add(this.professors.get(i).getContactInfo());
+        }
+
+        return contactInfo;
+
+    }
+
+    public boolean userExists(String userId) {
+        // COMPLETE-ME
+        // Verifique se existe o userId na lista de estudantes ou de professores
+
+        for (int i = 0; i < this.professors.size(); i++) {
+
+            if (this.professors.get(i).getUserId() == userId) {
+                return true;
+            }          
+        }
+
+        return false;
+    }
+
+    public int countMembers() {
+        // COMPLETE-ME
+        // Retorne o total de membros do laboratório (estudantes e professores)
+        return this.professors.size() + this.students.size();
+    }
 
 
 }
